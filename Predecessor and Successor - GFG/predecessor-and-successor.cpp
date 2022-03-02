2847dd01c675380b7a25dd497d9b489c01a1ec10
Tanaya Wankar <tanayawankar58@gmail.com>
2022-03-02 22:08:41 +0530

    Added solution - LeetHub

--- Predecessor and Successor - GFG/predecessor-and-successor.cpp
+++ Predecessor and Successor - GFG/predecessor-and-successor.cpp
@@ -107,8 +107,6 @@ struct Node
 // This function finds predecessor and successor of key in BST.
 // It sets pre and suc as predecessor and successor respectively
 
-
-
 void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
 {
     // Base case
@@ -148,4 +146,6 @@ void findPreSuc(Node* root, Node*& pre, Node*& suc, int key)
         pre = root ;
         findPreSuc(root->right, pre, suc, key) ;
     }
+    
+
 }
