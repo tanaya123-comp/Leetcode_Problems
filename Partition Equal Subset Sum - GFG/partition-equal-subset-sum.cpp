d7366b13fd669a9d560d3de0614aace1562aa83b
Tanaya Wankar <tanayawankar58@gmail.com>
2022-03-07 16:00:15 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -10,28 +10,28 @@ using namespace std;
 class Solution{
 public:
 
-    int DP[1001][1001];
+    // int DP[1001][1001];
 
-    int equalPartDP(int N,int arr[],int sum)
-    {
-        if(sum==0)
-        {
-            return DP[N][sum]=1;
-        }
-        if(N==0)
-        {
-            return DP[N][sum]=0;
-        }
-        if(DP[N][sum]!=-1)
-        {
-            return DP[N][sum];
-        }
-        if(arr[N-1]<=sum)
-        {
-            return DP[N][sum]=equalPartDP(N-1,arr,sum-arr[N-1])||equalPartDP(N-1,arr,sum);
-        }
-        return DP[N][sum]=equalPartDP(N-1,arr,sum);
-    }
+    // int equalPartDP(int N,int arr[],int sum)
+    // {
+    //     if(sum==0)
+    //     {
+    //         return DP[N][sum]=1;
+    //     }
+    //     if(N==0)
+    //     {
+    //         return DP[N][sum]=0;
+    //     }
+    //     if(DP[N][sum]!=-1)
+    //     {
+    //         return DP[N][sum];
+    //     }
+    //     if(arr[N-1]<=sum)
+    //     {
+    //         return DP[N][sum]=equalPartDP(N-1,arr,sum-arr[N-1])||equalPartDP(N-1,arr,sum);
+    //     }
+    //     return DP[N][sum]=equalPartDP(N-1,arr,sum);
+    // }
 
     int equalPartition(int N, int arr[])
     {
@@ -47,14 +47,38 @@ public:
         {
             return 0;
         }
-        for(i=0;i<1001;i++)
+        // for(i=0;i<1001;i++)
+        // {
+        //     for(j=0;j<1001;j++)
+        //     {
+        //         DP[i][j]=-1;
+        //     }
+        // }
+        sum=sum/2;
+        bool dp[N+1][sum+1];
+        for(i=0;i<=N;i++)
         {
-            for(j=0;j<1001;j++)
+            for(j=0;j<=sum;j++)
             {
-                DP[i][j]=-1;
+                if(j==0)
+                {
+                    dp[i][j]=true;
+                }
+                else if(i==0)
+                {
+                    dp[i][j]=false;
+                }
+                else if(arr[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
             }
         }
-        return equalPartDP(N,arr,sum/2);
+        return dp[N][sum];
     }
 };
 
