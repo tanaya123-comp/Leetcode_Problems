869d56f6bbcb6e62c76e7299e5bc105f777fe00f
Tanaya Wankar <tanayawankar58@gmail.com>
2022-03-27 16:33:50 +0530

    Added solution - LeetHub

--- Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
+++ Egg Dropping Puzzle - GFG/egg-dropping-puzzle.cpp
@@ -8,36 +8,34 @@ class Solution
     public:
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
-    int dp[201][201];
+    int dp[205][205];
     
-    int eggsDrop(int e,int f)
+    int eggDropRec(int e,int f)
     {
         if(dp[e][f]!=-1)
-        {
             return dp[e][f];
-        }
+            
         if(e==1)
-        {
             return dp[e][f]=f;
-        }
+            
         if(f<=1)
-        {
             return dp[e][f]=f;
-        }
-        
+            
         int ans=INT_MAX;
-        for(int i=1;i<=f;i++)
+        
+        for(int k=1;k<=f;k++)
         {
-            int temp=max(eggsDrop(e-1,i-1),eggsDrop(e,f-i));
+            int temp=max(eggDropRec(e-1,k-1),eggDropRec(e,f-k));
+            
             ans=min(ans,temp);
         }
+        
         return dp[e][f]=ans+1;
     }
     
-    int eggDrop(int e, int f) 
+    int eggDrop(int n, int k) 
     {
         // your code here
-        
         for(int i=0;i<=200;i++)
         {
             for(int j=0;j<=200;j++)
@@ -45,8 +43,8 @@ class Solution
                 dp[i][j]=-1;
             }
         }
-       return eggsDrop(e,f);
-	
+        
+      return  eggDropRec(n,k);
     }
 };
 
