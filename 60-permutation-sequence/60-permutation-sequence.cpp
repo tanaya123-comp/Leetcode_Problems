16bc833e1e11e46ab97d00a7732fa0c16916c742
Tanaya Wankar <tanayawankar58@gmail.com>
2022-04-13 10:29:33 +0530

    Time: 4 ms (43.93%), Space: 6.1 MB (17.32%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,62 +1,26 @@
 class Solution {
 public:
-    
-    string getNextPermutation(string s)
-    {
-        int i,j,n;
-        n=s.length();
+    string getPermutation(int n, int k) {
         
-        j=n-2;
-        while(j>=0&&(s[j+1]-'0')<(s[j]-'0'))
+        vector<int> numbers;
+        long fact=1;
+        for(int i=1;i<n;i++)    
         {
-            j--;
+            fact=fact*i;            //store factorial
+            numbers.push_back(i);   //store numbers
         }
-        
-       // cout<<j<<"\n";
-        
-        i=n-1;
-      //  j=j-1;
-        while(i>j&&(s[i]-'0')<(s[j]-'0'))
+        numbers.push_back(n);
+        k=k-1;      //make k zero based index
+        string ans="";
+        while(true)
         {
-            i--;
+            ans=ans+to_string(numbers[k/fact]);
+            numbers.erase(numbers.begin()+k/fact);
+            if(numbers.size()==0)
+                break;
+            k=k%fact;
+            fact=fact/numbers.size();
         }
-       // cout<<i<<"\n";
-        
-        swap(s[i],s[j]);
-        
-       // cout<<s<<"\n";
-        
-        i=n-1;
-        j=j+1;
-        while(j<=i)
-        {
-            swap(s[j],s[i]);
-            i--;
-            j++;
-        }
-        
-        //cout<<s<<"\n";
-        
-        return s;
-    }
-    
-    string getPermutation(int n, int k) 
-    {
-            
-        string s="";
-        
-        for(int i=1;i<=n;i++)
-        {
-            s=s+to_string(i);
-        }
-        
-     //   cout<<s<<"\n";
-        
-        for(int i=1;i<k;i++)
-        {
-            s=getNextPermutation(s);
-        }
-        
-        return s;
+        return ans;
     }
 };
