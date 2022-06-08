bbd54874ff0eaac1b8b31f02d9cb1b6422ba802e
Tanaya Wankar <tanayawankar58@gmail.com>
2022-06-08 20:01:57 +0530

    Time: 9 ms (10.15%), Space: 9.2 MB (30.76%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -6,31 +6,28 @@ public:
         i=m-1;
         j=n-1;
         k=m+n-1;
-        while(i>=0&&j>=0&&k>=i)
+        while(i>=0&&j>=0)
         {
-            if(nums1[i]<=nums2[j])
-            {
-                nums1[k]=nums2[j];
-                j--;
-                k--;
-            }
-            else
+            if(nums1[i]>nums2[j])
             {
                 nums1[k]=nums1[i];
                 i--;
                 k--;
             }
+            else
+            {
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
         }
         if(j>=0)
         {
-            i=i+1;
-            int l=0;
-            int r=j;
-            while(i<=k)
+            while(k>=0)
             {
-                nums1[i]=nums2[l];
-                i++;
-                l++;
+                nums1[k]=nums2[j];
+                j--;
+                k--;
             }
         }
         
