b1c4cfec0fe3508852afcb1c907afa295b51bfe5
Tanaya Wankar <tanayawankar58@gmail.com>
2022-07-08 10:19:37 +0530

    Time: 11 ms (94.59%), Space: 8.7 MB (74.34%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,37 +1,53 @@
+bool isEqual(int arr1[26],int arr2[26])
+{
+    for(int i=0;i<26;i++)
+    {
+        if(arr1[i]!=arr2[i])
+            return false;
+    }
+    return true;
+}
+
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         
-       int n,m,i,j;
-        n=s.length();
-        m=p.length();
-        vector<int> pv(26,0),sv(26,0);
-        vector<int> ans;
-        if(n<m)
+        int len1,len2,i,j,k;
+        len1=s.length();
+        len2=p.length();
+        
+        int arr1[26]={0};
+        int arr2[26]={0};
+        
+        for(i=0;i<len2;i++)
         {
-            return ans;
-        }
-        for(i=0;i<m;i++)
-        {
-            pv[p[i]-'a']++;
-            sv[s[i]-'a']++;
-        }
-        if(pv==sv)
-        {
-            ans.push_back(0);
+            arr2[p[i]-'a']++;
         }
         
-        for(i=p.size();i<n;i++)
+        vector<int> ans;
+        
+        i=0;
+        j=0;
+        while(j<len1)
         {
-            sv[s[i]-'a']++;
-            
-            sv[s[i-m]-'a']--;
-            
-            
-            if(pv==sv)
+            arr1[s[j]-'a']++;
+           // cout<<arr1<<" "<<arr2<<"\n";
+            if(isEqual(arr1,arr2))
             {
-                ans.push_back(i-m+1);
+                ans.push_back(i);
             }
+            
+            if(j-i+1>=len2)
+            {
+                while(j-i+1>=len2)
+                {
+                    arr1[s[i]-'a']--;
+                    i++;
+                }
+                
+            }
+            j++;
+            
         }
         
         return ans;
