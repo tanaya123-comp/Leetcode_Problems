a97a0568423efcb6cf761907854271065828d09b
Tanaya Wankar <tanayawankar58@gmail.com>
2022-08-30 12:10:40 +0530

    Time: 50 ms (62.92%), Space: 12.3 MB (83.14%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,43 +1,53 @@
 class Solution {
 public:
     
-void dfs(vector<vector<char>>& grid,vector<vector<bool> > &visited,int i,int j)
+    void numOfIslands(vector<vector<char>>& grid,int i,int j)
     {
-        visited[i][j]=true;
-        if((i-1)>=0&&visited[i-1][j]==false&&grid[i-1][j]=='1')
+        grid[i][j]='0';
+        
+        if((i-1)>=0&&grid[i-1][j]=='1')
         {
-            dfs(grid,visited,i-1,j);
+            numOfIslands(grid,i-1,j);
         }
-        if((i+1)<grid.size()&&visited[i+1][j]==false&&grid[i+1][j]=='1')
+        
+        if((j-1)>=0&&grid[i][j-1]=='1')
         {
-            dfs(grid,visited,i+1,j);
+            numOfIslands(grid,i,j-1);
         }
-        if((j-1)>=0&&visited[i][j-1]==false&&grid[i][j-1]=='1')
+        
+        if((i+1)<grid.size()&&grid[i+1][j]=='1')
         {
-            dfs(grid,visited,i,j-1);
+            numOfIslands(grid,i+1,j);
         }
-        if((j+1)<grid[0].size()&&visited[i][j+1]==false&&grid[i][j+1]=='1')
+        
+        if((j+1)<grid[0].size()&&grid[i][j+1]=='1')
         {
-            dfs(grid,visited,i,j+1);
+            numOfIslands(grid,i,j+1);
         }
+        
     }
+    
     int numIslands(vector<vector<char>>& grid) {
-       int n,m,i,j,ans;
+        
+        int n,m,i,j;
         n=grid.size();
         m=grid[0].size();
-        vector<vector<bool> > visited(n,vector<bool> (m,false));
-        ans=0;
+        
+        int num_of_islands=0;
+        
         for(i=0;i<n;i++)
         {
-            for(j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1'&&visited[i][j]==false)
-                {
-                    ans++;
-                    dfs(grid,visited,i,j);
-                }
+         for(j=0;j<m;j++)
+         {
+             if(grid[i][j]=='1')
+             {
+                num_of_islands++;
+             numOfIslands(grid,i,j);
             }
+             
+         }
         }
-        return ans;
+        
+        return num_of_islands;
     }
 };
